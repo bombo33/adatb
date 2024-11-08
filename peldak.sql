1504f277b713940b19ef1d26b6ded8a5c2796f24
Papp Botond <64477388+bombo33@users.noreply.github.com>
2024-11-08 10:57:13 +0100

    Update peldak.sql

--- peldak.sql
+++ peldak.sql
@@ -14,8 +14,7 @@ WHERE kiszállítási_idő IS NULL
   AND ügyfél_azonosító IN (
     SELECT ügyfél_azonosító 
     FROM ügyfél 
-    WHERE ország = 'Magyarország'
-  );
+    WHERE ország = 'Magyarország');
 
 -- 5. Rekordok törlése egy adott feltétel alapján.
 DELETE FROM ügyfél
@@ -23,7 +22,7 @@ WHERE státusz = 'Inaktív';
 
 -- 6. Adatok beszúrása egy kapcsolótáblába két EH-táblából származó adat alapján. Felvesszük azokat a tanulókat a kapcsolótáblába, akik 10. évfolyamba járnak és matematika kurzusra jelentkeztek.
 INSERT INTO rendelés (ügyfél_azonosító, áru_azonosító, dátum)
-SELECT u.ügyfél_azonosító, á.áru_azonosító, '2024-11-08'
-FROM ügyfél u, áru a
-WHERE ü.név = 'Kiss Péter'
-AND a.áru_azonosító = '12345';
+(SELECT u.ügyfél_azonosító, á.áru_azonosító, '2024-11-08'
+  FROM ügyfél u, áru a
+  WHERE ü.név = 'Kiss Péter'
+  AND a.áru_azonosító = '12345');
